5a21c1f05fc0e18f52da36bdbef5a3137726be43
Vincent Tourraine <me@vtourraine.net>
2022-12-03 21:47:28 +0100

    Migrate to Swift

--- generate.swift
+++ generate.swift
@@ -5,18 +5,25 @@ struct App {
     let iconPath: String
     let slug: String
     let releaseNotesPath: String
+    let description: String
+}
+
+extension App {
+    init(name: String, slug: String, description: String) {
+        self.init(name: name, iconPath: "apps/\(slug).png", slug: slug, releaseNotesPath: "~/Developer/\(slug)/releasenotes.json", description: description)
+    }
 }
 
 let apps: [App] = [
-    App(name: "Games Keeper", iconPath: "apps/gameskeeper.png", slug: "gameskeeper", releaseNotesPath: "~/Developer/GamesKeeper/releasenotes.json"),
-    App(name: "Comic Book Day", iconPath: "apps/comicbookday.png", slug: "comicbookday", releaseNotesPath: "~/Developer/comicbookday/releasenotes.json"),
-    App(name: "1List", iconPath: "apps/onelist.png", slug: "onelist", releaseNotesPath: "~/Developer/onelist/releasenotes.json"),
-    App(name: "Contact[s]", iconPath: "apps/contacts.png", slug: "contacts", releaseNotesPath: "~/Developer/contacts/releasenotes.json"),
-    App(name: "Mega Moji", iconPath: "apps/megamoji.png", slug: "megamoji", releaseNotesPath: "~/Developer/megamoji/releasenotes.json"),
-    App(name: "Memorii", iconPath: "apps/memorii.png", slug: "memorii", releaseNotesPath: "~/Developer/memorii/releasenotes.json"),
-    App(name: "Nano Notes", iconPath: "apps/nanonotes.png", slug: "nanonotes", releaseNotesPath: "~/Developer/nanonotes/releasenotes.json"),
-    App(name: "WizBox", iconPath: "apps/wizbox.png", slug: "wizbox", releaseNotesPath: "~/Developer/wizbox/releasenotes.json"),
-    App(name: "D0TS:Echoplex", iconPath: "apps/echoplex.png", slug: "echoplex", releaseNotesPath: "~/Developer/echoplex/releasenotes.json")
+    App(name: "Games Keeper", iconPath: "apps/gameskeeper.png", slug: "gameskeeper", releaseNotesPath: "~/Developer/GamesKeeper/releasenotes.json", description: "Score tracking for board games"),
+    App(name: "Nano Notes", slug: "nanonotes", description: "Sync notes with Apple Watch"),
+    App(name: "Comic Book Day", slug: "comicbookday", description: "Track comic book releases"),
+    App(name: "1List", slug: "onelist", description: "Simple reminders list"),
+    App(name: "Contact[s]", slug: "contacts", description: "Beautiful address book"),
+    App(name: "Mega Moji", slug: "megamoji", description: "Big emoji for iMessage"),
+    App(name: "Memorii", slug: "memorii", description: "Study Chinese, Japanese, and Korean"),
+    App(name: "WizBox", slug: "wizbox", description: "Magic: The Gathering toolbox"),
+    App(name: "D0TS:Echoplex", slug: "echoplex", description: "Music sequencer")
 ]
 
 /// FROM Trackup
@@ -71,7 +78,10 @@ let appsHTML = apps.map { app in
     return """
         <a class="app" href="https://www.studioamanga.com/\(app.slug)/">
             <img alt="\(app.name) Icon" class="icon" src="img/\(app.iconPath)">
-            <h3>\(app.name)</h3>
+            <div>
+                <h3>\(app.name)</h3>
+                <div class="subtitle">\(app.description)</div>
+            </div>
         </a>
     """ }.joined(separator: "\n")
 
